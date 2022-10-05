4cafe8a991885ed936fa4c2de9acec5fffc52a71
Sergey Vlasov <sergeyvlasov.pro@gmail.com>
2022-10-05 10:26:59 +0000

    add setup-env.sh

---
+++ setup-env.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# ln -s /usr/bin/python3 /usr/bin/python
+
+alias_ll="alias ll='ls -al'"
+alias_pip="alias pip='python -m pip'"
+
+add_alias_ll="echo \"${alias_ll}\" >> ~/.bash_aliases"
+add_alias_pip="echo \"${alias_pip}\" >> ~/.bash_aliases"
+
+cat ~/.bash_aliases
+echo "-----------------------------------------------------------------------------------"
+[[ -z $(grep "${alias_ll}" ~/.bash_aliases) ]] && eval "${add_alias_ll}" || :
+[[ -z $(grep "${alias_pip}" ~/.bash_aliases) ]] && eval "${add_alias_pip}" || :
+
+cat ~/.bash_aliases
+source ~/.bashrc
