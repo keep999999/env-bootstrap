90204f4b87cb3d190aa67fa920158dbb4412b1f6
Sergey Vlasov <sergeyvlasov.pro@gmail.com>
2022-10-05 22:01:51 +0000

    minor modifictions

---
+++ makefile
@@ -0,0 +1,2 @@
+start:
+	/usr/bin/python3.9 /com.docker.devenvironments.code/src/tgbot/bot.py
--- src/tgbot/bot.py
+++ src/tgbot/bot.py
@@ -1,13 +1,24 @@
 from telegram import Update
 from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
 from settings import TELEGRAM_TOKEN
+import structlog
+
+logger = structlog.get_logger()
 
 async def hello(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    msg = f'Hello {update.effective_user.first_name}'
+    logger.debug(msg)
     await update.message.reply_text(f'Hello {update.effective_user.first_name}')
 
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    return await hello(update=update, context=context)
 
-app = ApplicationBuilder().token(TELEGRAM_TOKEN).build()
 
-app.add_handler(CommandHandler("hello", hello))
+if __name__ == "__main__":
+    
+    app = ApplicationBuilder().token(TELEGRAM_TOKEN).build()
 
-app.run_polling()
+    app.add_handler(CommandHandler("start", start))
+    app.add_handler(CommandHandler("hello", hello))
+
+    app.run_polling()
